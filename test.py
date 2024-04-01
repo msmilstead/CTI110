2e01f94e59ffa41ff4a74222a831729af07e7e68
msmilstead <165720750+msmilstead@users.noreply.github.com>
2024-04-01 16:59:00 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+
+
+for variable in range(5):
+    print(variable)
+    #print(f"Loop is on turn #{variable})
+    
+'''
+num = int(input("Enter a number less than 10: "))
+
+#While the number is greater than 10, make the user enter another number
+while num > 10:
+    print("Number must be less than 10")
+    num = int(input("Enter a number less than 10: "))
+''' 
